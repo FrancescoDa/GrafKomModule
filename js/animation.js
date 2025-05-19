b5c9dce9f7a6ffc07f394acc20be0418ad7a5491
Eelleenn <francesco.d23@mhs.istts.ac.id>
2025-05-19 11:46:19 +0700

    menghilangkan rotasi

--- js/animation.js
+++ js/animation.js
@@ -12,19 +12,19 @@ export function startAnimationLoop(
     controls.update(); // Penting untuk OrbitControls jika damping aktif
 
     // Opsional: Tambahkan animasi pada objek jika diinginkan
-    if (gameObjects && gameObjects.cube) {
-      gameObjects.cube.rotation.x += 0.001;
-      gameObjects.cube.rotation.y += 0.002;
-    }
-    if (gameObjects && gameObjects.sphere) {
-      gameObjects.sphere.rotation.y -= 0.003;
-    }
-    if (gameObjects && gameObjects.pyramid) {
-      gameObjects.pyramid.rotation.y += 0.001;
-    }
-    if (gameObjects && gameObjects.arrowGroup) {
-      gameObjects.arrowGroup.rotation.y += 0.01;
-    }
+    // if (gameObjects && gameObjects.cube) {
+    //   gameObjects.cube.rotation.x += 0.001;
+    //   gameObjects.cube.rotation.y += 0.002;
+    // }
+    // if (gameObjects && gameObjects.sphere) {
+    //   gameObjects.sphere.rotation.y -= 0.003;
+    // }
+    // if (gameObjects && gameObjects.pyramid) {
+    //   gameObjects.pyramid.rotation.y += 0.001;
+    // }
+    // if (gameObjects && gameObjects.arrowGroup) {
+    //   gameObjects.arrowGroup.rotation.y += 0.01;
+    // }
 
     renderer.render(scene, camera);
   }
